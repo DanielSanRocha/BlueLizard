83e4851d035fe0f96debb5bd17bf3178f7d4149a
Daniel Santana <danielsantanarocha@gmail.com>
2018-09-21 17:44:01 -0300

    Registered the new numeric input components globally.
    Addded a navleft that will be defined later.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <template>
   <div id="app">
+    <navleft/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import navleft from '@/components/navleft'
+import VueNumeric from 'vue-numeric'
+import VueNumberInput from '@chenfengyuan/vue-number-input'
+const NeighborJoining = require('./algorithms/NeighborJoining')
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {navleft},
+  created: function () {
+    Vue.use(VueNumeric)
+    Vue.component(VueNumberInput.name, VueNumberInput)
+  },
+  methods: {
+    NeighborJoining: function (matrix) {
+      NeighborJoining.apply(matrix)
+    }
+  }
 }
 </script>
 
 <style>
+
+body {
+  margin: 0px;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
 }
 </style>
