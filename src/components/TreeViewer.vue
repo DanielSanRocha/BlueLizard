9a5bfe08519e47157cdf447099f174199d7f1745
Daniel Santana Rocha <danielsantanarocha@gmail.com>
2018-10-02 02:07:49 -0300

    Added a example network.

--- src/components/TreeViewer.vue
+++ src/components/TreeViewer.vue
@@ -1,34 +1,38 @@
 <template>
   <div id="TreeViewer">
-    <timeline :items="items" :groups="groups" :options="options"></timeline>
+    <network class="network" :nodes="nodes" :edges="edges"></network>
   </div>
 </template>
 
 <script>
+import * as vue2vis from 'vue2vis'
+
 export default {
   name: 'TreeViewer',
+  components: {'network': vue2vis.Network},
   data () {
     return {
-      groups: [{
-        id: 0,
-        content: 'Group 1'
-      }],
-      items: [{
-        id: 0,
-        group: 0,
-        start: new Date(),
-        content: 'Item 1'
-      }],
-      options: {
-        editable: true
-      }
+      nodes: [
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+      ],
+      edges: [
+        {from: 1, to: 3},
+        {from: 1, to: 2},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
-#TreeViewer {
-
+.network {
+  height: 100%;
+  width: 100%;
 }
 </style>
